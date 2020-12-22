84aebb2683fd650f3252a31a933312ee18f2b5b2
Jeongum <aldlswjddma@naver.com>
2020-12-22 14:42:38 +0900

    complete truffle && fix app.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,37 +1,3 @@
-import logo from './logo.svg';
-import './App.css';
-import {HashRouter, Route} from 'react-router-dom';
-import Main from './routes/Main';
-import MyShareList from './routes/MyShareList';
-import MyPage from'./routes/MyPage';
-import MileageApplication from'./routes/MileageApplication';
-import ShareItemList from './routes/ShareItemList';
-import ShareItemDetail from './routes/ShareItemDetail';
-import ShareTalentList from './routes/ShareTalentList';
-import ShareTalentDetail from './routes/ShareTalentDetail';
-import Navigation from './components/Navigation';
-
-function App() {
-  let router;
-  const navigate = (pageName = '') =>{
-    router.history.push('/'+pageName);
-
-
-  return (
-      <HashRouter ref = {(r)=>{router = r;}}>
-        <Route path="/" exact={true} component={Main}/>
-        <Route path="/mysharelist" component={MyShareList}/>
-        <Route path="/mileage_application" component={MileageApplication}/>
-        <Route path="/mypage" component={MyPage}/>
-
-        <Route path="/share_item_list" component={ShareItemList}/>
-        <Route path="/share_item_detail" component={ShareItemDetail}/>
-        <Route path="/share_talent_list" component={ShareTalentList}/>
-        <Route path="/share_talent_detail" component={ShareTalentDetail}/>
-      </HashRouter>
-  );
-}
-
 import React, { Component } from "react";
 import MyNFTContract from "./contracts/MyNFT.json";
 import AuctionsContract from "./contracts/Auctions.json";
@@ -44,6 +10,7 @@ class App extends Component {
     super(props);
 
     this.state = {
+      defaultUser: null,
       web3: null
     };
   }
@@ -63,15 +30,17 @@ class App extends Component {
   }
 
   instantiateContract() {
-    const contract = require("truffle-contract");
-    const MyNFT = contract(MyNFTContract);
-    const Auctions = contract(AuctionsContract);
-    
-    MyNFT.setProvider(this.state.web3.currentProvider);
-    Auctions.setProvider(this.state.web3.currentProvider);
-    
+    this.state.web3.eth.getAccounts((error, accounts) => {
+      if (!error) {
+        this.setState({ myAccount: accounts[0] });
+      }
+    });
   }
 
   render() {
-    return <div className="App">Fruit shop</div>;
+      return <div>{this.state.defaultUser}</div>
   }
+
+}
+
+export default App;
