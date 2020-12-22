51193e7c01de820b7a9b475c855cd74f14983e92
chaeso7279 <chaeso7279@gmail.com>
2020-12-22 12:39:01 +0900

    빠진거 다시 추가

--- client/src/index.js
+++ client/src/index.js
@@ -2,11 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
+import * as serviceWorker from './serviceWorker';
 
 import { MuiThemeProvider, createMuiTheme } from '@material-ui/core/styles';
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
 
 ReactDOM.render(
 
     <App />
   , document.getElementById('root')
 );
+
+
